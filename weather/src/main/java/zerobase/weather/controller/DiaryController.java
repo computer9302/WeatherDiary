5c20032ed8f5360815ec2f3373b40f8706912d04
ParkJunYoung <computer9302@gmail.com>
2024-09-02 16:04:42 +0900

    Last commit

--- weather/src/main/java/zerobase/weather/controller/DiaryController.java
+++ weather/src/main/java/zerobase/weather/controller/DiaryController.java
@@ -1,5 +1,9 @@
 package zerobase.weather.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.cglib.core.Local;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
@@ -18,20 +22,23 @@ public class DiaryController {
         this.diaryService = diaryService;
     }
 
+    @Operation(summary = "여기에 말을 치면 어디에 뜰까?", description = "이것은 노트")
     @PostMapping("/create/diary")
     void createDiary(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) LocalDate date, @RequestBody String text) throws MalformedURLException {
         diaryService.createDiary(date, text);
 
     }
 
+    @Operation(summary = "선택한 날씨의 모든 일기 데이터를 가져옵니다")
     @GetMapping("/read/diary")
     List<Diary> readDiary(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE)LocalDate date){
        return diaryService.readDiary(date);
     }
 
+    @Operation(summary = "선택한 기간중의 모든 일기 데이터를 가져옵니다")
     @GetMapping("/read/diaries")
-    List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) LocalDate startDate,
-                            @RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) LocalDate endDate){
+    List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) @Parameter(description = "조회할 기간의 마지막날", example = "2020-02-02") LocalDate startDate,
+                            @RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) @Parameter(description = "조회할 기간의 마지막날", example = "2020-02-02") LocalDate endDate){
         return diaryService.readDiaries(startDate, endDate)
 ;    }
 
